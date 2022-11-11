b9ebd404f4b4982c4aafb25bf2720809a099fa7e
Djay <69097682+Djay47@users.noreply.github.com>
2022-11-11 13:21:01 +0700

    Add files via upload

---
+++ proyek-restoran/pelanggan/confirm-delete.php
@@ -0,0 +1,20 @@
+<?php
+	$idpelanggan = $_GET["id"];
+?>
+
+<h2>Konfirmasi Hapus Data</h2>
+<hr>
+<table class="table-sm">
+	<tr>
+		<td>
+			<a href="?dir=pelanggan&file=select" type="button" class="btn btn-outline-danger btn-sm py-0">
+				Batal
+			</a>
+		</td>
+		<td>
+			<a href="?dir=pelanggan&file=delete&id=<?= $idpelanggan ?>" type="button" class="btn btn-outline-success btn-sm py-0">
+				Lanjut
+			</a>
+		</td>
+	</tr>
+</table>
