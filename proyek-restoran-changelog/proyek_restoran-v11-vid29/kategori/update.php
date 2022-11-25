d8715d2a3dec192525ee3f89edfa84b2e1be4a06
Djay <69097682+Djay47@users.noreply.github.com>
2022-11-25 13:47:35 +0700

    Add files via upload

---
+++ proyek-restoran-changelog/proyek_restoran-v11-vid29/kategori/update.php
@@ -0,0 +1,38 @@
+<h2>Perbarui</h2>
+
+<hr class="my-1">
+
+<a type="button" class="btn btn-outline-dark btn-sm py-0 m-1" href="?dir=kategori&file=select">Kembali</a>
+
+<hr class="my-1">
+
+<?php
+	$id = $_GET["id"];
+	$kategori = $database->query_select("SELECT kategori FROM kategori WHERE idkategori = $id");
+?>
+
+<form action="" method="post" class="mt-3">
+	<input type="hidden" name="id" value="<?= $id ?>">
+	<table class="table-sm">
+		<th><label for="kategori">Kategori:</label></th>
+		<td>
+			<input type="text" name="kategori" value="<?= $kategori[0]["kategori"] ?>" placeholder="<?= $kategori[0]["kategori"] ?>" required>
+		</td>
+		<td><button type="submit" name="simpan" class="btn btn-success btn-sm">Simpan</buton></td>
+	</table>
+</form>
+
+<?php
+	if ( isset($_POST["simpan"]) )
+	{
+		if ( $database->query_update($_POST) >= 0 )
+		{
+			echo "Query Ok!<br>";
+		}
+		else
+		{
+			echo "Query Failed!<br>";
+			echo mysqli_error($database->conn);
+		}
+	}
+?>
